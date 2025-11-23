38dc70128a666b39d193f6c8aaad6461a9708ab0
B00gor <Boogorkolenka@gmail.com>
2025-11-23 07:04:55 +0300

    картинки

--- build/unknown-Release/qtcsettings.cmake
+++ build/unknown-Release/qtcsettings.cmake
@@ -1,10 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/bin/g++" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/bin/gcc" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/boogor/untitled2/build/unknown-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
